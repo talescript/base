94340f94363a294d66233cdefcf87d52f2ab835c
talescript <teichopsia@gmail.com>
2018-03-21 01:36:40 -0500

    Done for today. Add cosmic latte to sections

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-
-/* // Firefox does not want to cooperate
-function detectChange(){
-    const height = Math.max(document.documentElement.clientHeight, 
-        window.innerHeight || 0);
-
-    const viewportHeight = document.querySelector('.hero--bg');
-    viewportHeight.style['min-height'] = height + 'px';
-    alert("workkkkking");
-}
-
-window.addEventListener("orientationchange", detectChange, false);  */
-
 // Modal
 const btn_modal = document.querySelector('.btn--modal');
 const close = document.querySelector('.close');
